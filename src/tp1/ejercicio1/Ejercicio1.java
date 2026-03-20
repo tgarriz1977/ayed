6284923491260b663d2431cc149828756546c28f
Juan Pablo Garriz <juan.garriz@arba.gov.ar>
2026-03-20 10:12:54 -0300

    un commit

--- src/tp1/ejercicio1/Ejercicio1.java
+++ src/tp1/ejercicio1/Ejercicio1.java
@@ -3,28 +3,28 @@ package tp1.ejercicio1;
 public class Ejercicio1 {
     public static void imprimeFor(int a, int b){
         for(int i=a; i<=b; i++){
-            System.out.println("valor: " + $i)
+            System.out.println("valor: " + i);
         }
     }
     public static void imprimeWhile(int a, int b){
         while (a <= b){
-            System.out.println("valor: " + $a);
+            System.out.println("valor: " + a);
             a++;
         }
     }
     public static void imprimeRecursivo(int a, int b){
         if (a > b) return;
-        System.out.println("valor: " + $a);
-        imprimeRecursivo(a++,b);
+        System.out.println("valor: " + a);
+        imprimeRecursivo(++a,b);
     }
     public static void main(String[] args) {
         System.out.println("Con for:");
-        imprimirFor(3, 7);
+        imprimeFor(3, 7);
 
         System.out.println("Con while:");
-        imprimirWhile(3, 7);
+        imprimeWhile(3, 7);
 
         System.out.println("Con recursión:");
-        imprimirRecursivo(3, 7);
+        imprimeRecursivo(3, 7);
     }
 }

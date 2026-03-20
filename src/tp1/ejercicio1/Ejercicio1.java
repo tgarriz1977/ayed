2181fa2ab6b01d534058e38727310254a0bca6fc
Juan Pablo Garriz <juan.garriz@arba.gov.ar>
2026-03-20 09:19:34 -0300

    un commit

---
+++ src/tp1/ejercicio1/Ejercicio1.java
@@ -0,0 +1,10 @@
+package tp1.ejercicio1;
+
+public class MiClase {
+    public static void imprimeFor(int a, int b){
+        for(i=a; i=b; i++){
+            System.out.print("valor: " + $i + "\n")
+        }
+    }
+
+}

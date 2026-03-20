117b9146bbf76e9f769cc5d65e7a08e1f3f815a2
Juan Pablo Garriz <juan.garriz@arba.gov.ar>
2026-03-20 09:27:58 -0300

    un commit

--- src/tp1/ejercicio1/Ejercicio1.java
+++ src/tp1/ejercicio1/Ejercicio1.java
@@ -3,13 +3,18 @@ package tp1.ejercicio1;
 public class MiClase {
     public static void imprimeFor(int a, int b){
         for(i=a; i=b; i++){
-            System.out.print("valor: " + $i + "\n")
+            System.out.println("valor: " + $i + "\n")
         }
     }
     public static void imprimeWhile(int a, int b){
         while (a <= b){
-            System.out.print("valor: " + $a + "\n");
+            System.out.println("valor: " + $a + "\n");
             a++;
         }
+    }
+    public static void imprimeRecursivo(int a, int b){
+        if (a > b) return;
+        System.out.println("valor: " + $a + "\n");
+        imprimeRecursivo(a++,b);
     }
 }

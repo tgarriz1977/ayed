781c20b2a0c248c420f6f842e92dd56d051c8948
juan <juan.garriz@sofis.lat>
2026-03-24 15:09:52 -0300

    add ej7h

--- src/tp1/ejercicio7/TestArrayList.java
+++ src/tp1/ejercicio7/TestArrayList.java
@@ -11,6 +11,30 @@ public class TestArrayList {
         }
         return lista.equals(aux);
     }
+    //7j
+    public ArrayList<Integer> combinarOrdenado(ArrayList<Integer> lista1, ArrayList<Integer> lista2){
+        int i=0,j=0;
+        ArrayList<Integer> result = new ArrayList<>();
+        while (i < lista1.size() && j< lista2.size()) {
+            if (lista1.get(i) <= lista2.get(j)) {
+                result.add(lista1.get(i));
+                i++;
+            } else {
+                result.add(lista2.get(j));
+                j++;
+            }
+        }
+        while (i < lista1.size()){
+            result.add(lista1.get(i));
+            i++;
+        }
+        while (j < lista2.size()){
+            result.add(lista2.get(j));
+            j++;
+        }
+        return result;
+    }
+
     public static void main(String[] args){
         List<Integer> lista = new ArrayList<>();
         for (String arg : args){

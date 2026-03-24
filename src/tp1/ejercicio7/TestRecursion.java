4d713ad8bb170e8989749ff12a5c49f8499df6a0
juan <juan.garriz@sofis.lat>
2026-03-24 11:03:42 -0300

    add ej7h

--- src/tp1/ejercicio7/TestRecursion.java
+++ src/tp1/ejercicio7/TestRecursion.java
@@ -1,10 +1,21 @@
 package tp1.ejercicio7;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestRecursion {
     public static void main(String[] args) {
+        // prueba ej 7g
+        //EjercicioRecursion ej = new EjercicioRecursion();
+        //List<Integer> lista = ej.calcularSucesion(56);
+        //lista.forEach(el -> System.out.println(el));
+
+        //Prueba ej 7h
         EjercicioRecursion ej = new EjercicioRecursion();
-        List<Integer> lista = ej.calcularSucesion(56);
-        lista.forEach(el -> System.out.println(el));
+        ArrayList<Integer> lista = new ArrayList<>();
+        for(int i=0;i<=9;i++){
+            lista.add(i);
+        }
+        ej.invertirArrayList(lista);
+        lista.forEach(a -> System.out.println(a));
     }
 }
